e55898561fd523b1dad49c5e54a093c334313542
Munish <hmunish5@gmail.com>
2024-11-06 21:55:40 +0530

    Initiate nextjs framework

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/app/shared/styles")],
+    prependData: `
+          @import '@sharedStyles/mixins.scss';
+        `,
+  },
+  webpack: (config) => {
+    config.resolve.alias["@sharedStyles"] = path.join(
+      __dirname,
+      "src/app/shared/styles"
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
