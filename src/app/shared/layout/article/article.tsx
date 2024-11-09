e5bc49ff15b80c047e689a67108808e3ba9c03e1
Munish <hmunish5@gmail.com>
2024-11-09 11:13:50 +0530

    Fix issuse of navbar hiding on page scroling into view

--- src/app/shared/layout/article/article.tsx
+++ src/app/shared/layout/article/article.tsx
@@ -8,19 +8,16 @@ export default function Article({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const contentWrapperRef = useRef<HTMLDivElement>(null);
+  const profileContentWrapperRef = useRef<HTMLDivElement>(null);
   const scrollToContent = () => {
-    const yOffset = -62;
-    const y = (contentWrapperRef.current?.getBoundingClientRect().top || 0) + window.pageYOffset + yOffset;
-    contentWrapperRef.current?.scrollIntoView({ behavior: "smooth" });
-    window.scrollTo({ top: y, behavior: "smooth" });
+    profileContentWrapperRef.current?.scroll({top: 530, behavior: "smooth"})
   };
   return (
     <section id="article">
       <Navbar onScrollToContent={scrollToContent}></Navbar>
-      <div id="profile_content_wrapper">
+      <div ref={profileContentWrapperRef} id="profile_content_wrapper">
         <Profile onScrollToContent={scrollToContent}></Profile>
-        <div ref={contentWrapperRef} id="content_wrapper">
+        <div  id="content_wrapper">
           {children}
         </div>
       </div>
